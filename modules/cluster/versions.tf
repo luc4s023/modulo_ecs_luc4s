34c7190726a3f1d0515138fa634803728ccb924c
luc4s023 <lucas.s023@outlook.com>
2026-01-18 16:50:12 -0300

    Fix AWS provider version constraint to >= 5.0

--- modules/cluster/versions.tf
+++ modules/cluster/versions.tf
@@ -1,20 +1,10 @@
 terraform {
-  required_version = ">= 1.5.7"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.28"
+      version = ">= 5.0"
     }
-    time = {
-      source  = "hashicorp/time"
-      version = ">= 0.13"
-    }
-  }
-
-  provider_meta "aws" {
-    user_agent = [
-      "github.com/terraform-aws-modules/terraform-aws-ecs"
-    ]
   }
 }
--- modules/service/versions.tf
+++ modules/service/versions.tf
@@ -1,16 +1,10 @@
 terraform {
-  required_version = ">= 1.5.7"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.28"
+      version = ">= 5.0"
     }
-  }
-
-  provider_meta "aws" {
-    user_agent = [
-      "github.com/terraform-aws-modules/terraform-aws-ecs"
-    ]
   }
 }
